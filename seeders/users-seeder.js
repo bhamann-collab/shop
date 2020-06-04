0f48f6b73f1001490b196e47ef300db4a32530d1
bhamann-collab <brockohamann@gmail.com>
2020-06-04 15:08:24 +0930

    Updated database entries, now have columns: password, lastName and email

--- seeders/users-seeder.js
+++ seeders/users-seeder.js
@@ -5,22 +5,33 @@ module.exports = {
 
     return queryInterface.bulkInsert('users', [{
         name: 'Brock',
+        lastName: 'Hamann',
+        email: 'brock@brock.com',
+        password: 'L)u`uc35?+',
         createdAt: new Date(),
         updatedAt: new Date()
     }, {
         name: 'Matt',
+        lastName: 'Teague',
+        password: 'GW--d987UX',
         createdAt: new Date(),
         updatedAt: new Date()
     },{
         name: 'Claire',
+        lastName: 'Vandenberg',
+        password: '{gy8WwHm2U',
         createdAt: new Date(),
         updatedAt: new Date()
     },{
         name: 'Angelo',
+        lastName: 'Ungos',
+        password: 'cZN@:&d8y',
         createdAt: new Date(),
         updatedAt: new Date()
     },{
-        name: 'Damien',
+        name: 'Damian',
+        lastName: 'Tapp',
+        password: '!4R&-du2gQ',
         createdAt: new Date(),
         updatedAt: new Date()
     }]);
