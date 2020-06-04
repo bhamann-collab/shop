505ec5bfac45d9ef8bc629a568a0165b87646f99
bhamann-collab <brockohamann@gmail.com>
2020-06-04 16:51:16 +0930

    Can now see data from a user and the current items the user has put in their shopping cart

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -1,18 +1,42 @@
 //Controllers handle the behaviour between user input and program actions. In this case it is referring user routes to database queries using the burger model.
 
 //Required dependencies.
-let {saleItems} = require("../models");
+let db = require("../models");
 
-console.log(saleItems)
+console.log(db.users)
 
 module.exports = function (app) {
-    app.get("/", function (req, res) {//the home route
-        saleItems.findAll().then((result) => {
+    app.get("/", function (req, res) {
 
-            console.log(result)
+        //two pieces of data to be sent to the root file
+
+        
+        db.saleItems.findAll().then((result) => {
+
+            //console.log({result})
 
              //sends object to handlebars for display (conversion was required as handlebars can only handle objects)
-            res.render("index", {data: result});
+            //res.render("index", {itemsForSale: result});
+        })
+
+        db.users.findAll({
+            //Test case, using user id: 2 for now
+            where: {id: 2 },
+            include: [{
+                model: db.orders,
+                //Test case, using user id: 1 for now
+                where: {id: 11},
+                include: [{
+                    model: db.items,
+                    where: {},
+                    include: [{
+                        model: db.saleItems,
+                        where:{}
+                    }]
+                }]
+            }]
+        }).then((result) => {
+            res.render("index", {user: result});
         })
     });
 };
