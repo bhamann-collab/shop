cd42ccc971b651fc50ce915d9564eefb4d71cdb2
bhamann-collab <brockohamann@gmail.com>
2020-06-10 15:41:31 +0930

    can now view total price

--- server.js
+++ server.js
@@ -30,7 +30,10 @@ app.engine("handlebars", exphbs({
     helpers: {
         //Custom helper to show the total price of the order
         getTotal: function (shoes){
-            let total = shoes.reduce(function(a, b) { return a + b.dataValues.saleItem.dataValues.price; }, 0)
+            let total = shoes.reduce(function(a, b) { 
+                let totalVal = +(a + b.dataValues.saleItem.dataValues.price - 23.65).toFixed(2)
+                return totalVal; 
+            }, 0)
             return total
         }
     }
